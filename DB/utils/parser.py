9a42978003e0e2f22f43a3f095e0f29ed2b98384
tom-nachum <tom.nachum@gmail.com>
2022-10-15 16:43:11 +0300

    created set of pokemons and trainers id's

--- DB/utils/parser.py
+++ DB/utils/parser.py
@@ -12,16 +12,20 @@ def _get_dict_from_json(json_dir: str):
 
 
 def _parse_poke_data(poke_data):
-    pokemons, trainers = set(), set()
+    pokemons, trainers, pokemons_trainers = set(), set(), set()
     trainer_id = 1
     for p in poke_data:
-        pokemon = Pokemon(p[P_ID], p[P_NAME], p[P_TYPE], p[P_HEIGHT], p[P_WEIGHT])
+        p_id = p[P_ID]
+        pokemon = Pokemon(p_id, p[P_NAME], p[P_TYPE], p[P_HEIGHT], p[P_WEIGHT])
         pokemons.add(pokemon)
+        if len(p[P_TRAINER]) == 0:
+            pokemons_trainers.add((p_id, None))
         for t in p[P_TRAINER]:
             trainer = Trainer(trainer_id, t[T_NAME], t[T_TOWN])
             trainers.add(trainer)
+            pokemons_trainers.add((p_id, trainer_id))
             trainer_id += 1
-    return pokemons, trainers
+    return pokemons, trainers, pokemons_trainers
 
 
 def parse(json_dir: str):
