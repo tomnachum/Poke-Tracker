ad30366ed7526e194822a56a8449a82f8659aa87
tom-nachum <tom.nachum@gmail.com>
2022-10-18 16:04:34 +0300

    updated types: created a route for get pokemon by name and update the DB with additional types from pokeAPI

---
+++ queries.py
@@ -0,0 +1,82 @@
+import pymysql
+
+connection = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    db="poke_tracker",
+    charset="utf8",
+    cursorclass=pymysql.cursors.DictCursor,
+)
+
+
+def get_pokemon_id(pokemon_name: str):
+    try:
+        with connection.cursor() as cursor:
+            query = f"""
+                    SELECT p_id
+                    FROM pokemons
+                    WHERE name = '{pokemon_name}'
+                    """
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result[0]["p_id"]
+    except Exception as e:
+        print(e)
+
+
+def get_types():
+    try:
+        with connection.cursor() as cursor:
+            query = f"""
+                    SELECT *
+                    FROM types
+                    """
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return {t["name"]: t["ty_id"] for t in result}
+    except Exception as e:
+        print(e)
+
+
+def add_type(type_name: str):
+    if type_name in get_types():
+        return
+    try:
+        with connection.cursor() as cursor:
+            query = f"""
+                    INSERT INTO types VALUES
+                    (null, '{type_name}')
+                    """
+            cursor.execute(query)
+            connection.commit()
+    except Exception as e:
+        print(e)
+
+
+def add_pokemon_type_pair(p_id, ty_id):
+    try:
+        with connection.cursor() as cursor:
+            query = f"""
+                    INSERT INTO pokemons_types VALUES
+                    ({p_id}, {ty_id})
+                    """
+            cursor.execute(query)
+            connection.commit()
+    except Exception as e:
+        print(e)
+
+
+def get_type_id(type_name):
+    try:
+        with connection.cursor() as cursor:
+            query = f"""
+                    SELECT ty_id
+                    FROM types
+                    WHERE name = '{type_name}'
+                    """
+            cursor.execute(query)
+            result = cursor.fetchall()
+            return result[0]["ty_id"]
+    except Exception as e:
+        print(e)
---
+++ server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request, Response
+import uvicorn
+import requests
+from queries import *
+
+app = FastAPI()
+
+
+@app.get("/pokemons/{name}")
+def get_pokemon(name: str):
+    pokemon_response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}").json()
+    types = [e["type"]["name"].lower() for e in pokemon_response["types"]]
+    p_id = get_pokemon_id(name)
+    for type in types:
+        add_type(type)
+        ty_id = get_type_id(type)
+        add_pokemon_type_pair(p_id, ty_id)
+    # TODO: return the pokemon data
+    return {"id": p_id, "types": types}
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
